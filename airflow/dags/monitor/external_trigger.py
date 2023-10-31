5035c0cb72b87ffed9481734e159137d43df33c0
Pankaj <pankaj.singh@astronomer.io>
2023-11-01 01:11:32 +0530

    Pulish monitoring result when fail and once a day

--- airflow/dags/monitor/external_trigger.py
+++ airflow/dags/monitor/external_trigger.py
@@ -1,11 +1,10 @@
 from datetime import datetime
+
 from airflow.decorators import dag
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 
-@dag(
-    schedule_interval="@daily", start_date=datetime(2023, 9, 27), catchup=False, is_paused_upon_creation=True
-)
+@dag(schedule_interval="@daily", start_date=datetime(2023, 9, 27), catchup=False, is_paused_upon_creation=True)
 def external_trigger_monitoring_dag():
     TriggerDagRunOperator(
         task_id="run_monitoring_dag",
