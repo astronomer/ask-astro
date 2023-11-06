3808ace2b437dea3751ee7536aa6a7e20740e915
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2023-11-06 13:17:34 +0545

    Add test for on_list_recent_requests

---
+++ tests/api/ask_astro/rest/controllers/test_list_recent_requests.py
@@ -0,0 +1,59 @@
+from unittest.mock import AsyncMock, Mock, PropertyMock, patch
+
+import pytest
+from sanic import Sanic
+from sanic_testing import TestManager
+
+sanitized_name = __name__.replace(".", "_")
+
+
+@pytest.fixture
+def app() -> Sanic:
+    app_instance = Sanic(sanitized_name)
+    TestManager(app_instance)
+    from ask_astro.rest.controllers import register_routes
+
+    register_routes(app_instance)
+    return app_instance
+
+
+def generate_mock_document(data):
+    mock_document = Mock()
+    mock_document.to_dict.return_value = data
+    return mock_document
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "mock_data,expected_status,expected_response",
+    [
+        ([], 500, {"error": "An error occurred while processing your request."}),
+    ],
+)
+async def test_on_list_recent_requests(app, mock_data, expected_status, expected_response):
+    with patch("ask_astro.config.FirestoreCollections.requests", new_callable=PropertyMock) as mock_collection:
+        mock_collection.return_value = "mock_collection_name"
+        print("Setting up mock")
+        with patch("google.cloud.firestore_v1.Client", new=AsyncMock()) as MockFirestoreClient:
+            # Here, MockFirestoreClient will replace the actual Firestore Client everywhere in the code.
+            mock_client_instance = MockFirestoreClient.return_value
+
+            # Create the final result (from Firestore's get() method)
+            mock_get = AsyncMock()
+            mock_get.return_value = [generate_mock_document(doc) for doc in mock_data]
+
+            # Mock the Firestore query methods
+            mock_query = AsyncMock()
+            mock_query.order_by.return_value = mock_query
+            mock_query.where.return_value = mock_query
+            mock_query.limit.return_value = mock_query
+            mock_query.get = mock_get
+
+            # Mock the Firestore collection call
+            mock_client_instance.collection.return_value = mock_query
+
+            _, response = await app.asgi_client.get("/requests")
+            print(response.content)
+
+            assert response.status == expected_status
+            assert response.json == expected_response
