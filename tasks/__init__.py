e41b8d42a06d3cc527e1e6022998b588fed02422
Wei Lee <weilee.rx@gmail.com>
2023-11-07 16:59:58 +0800

    build(tasks): add ui tasks

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,7 +1,8 @@
 from invoke import Collection
 
-from tasks import api, docs
+from tasks import api, docs, ui
 
 ns = Collection()
 ns.add_collection(api)
 ns.add_collection(docs)
+ns.add_collection(ui)
