a266ca73373917feae07aaace017b8074fc8f0b6
mpgreg <michaelgregory122@gmail.com>
2023-11-13 13:11:53 +0100

    fixes #110

--- airflow/include/tasks/ingest.py
+++ airflow/include/tasks/ingest.py
@@ -30,12 +30,16 @@ def import_upsert_data(dfs: list[pd.DataFrame], class_name: str, primary_key: st
 
     df["uuid"] = df.apply(lambda x: generate_uuid5(identifier=x.to_dict(), namespace=class_name), axis=1)
 
+    if df[["docLink", "uuid"]].duplicated().any():
+        df.drop_duplicates(subset=["docLink", "uuid"], keep="first", inplace=True)
+        df.reset_index(drop=True, inplace=True)
+
     print(f"Passing {len(df)} objects for import.")
 
     return {
         "data": df,
         "class_name": class_name,
-        "upsert": True,
+        "existing": "upsert",
         "primary_key": primary_key,
         "uuid_column": "uuid",
         "error_threshold": 0,
@@ -52,7 +56,7 @@ def import_data(dfs: list[pd.DataFrame], class_name: str) -> list:
     A 'uuid' is generated based on the content and metadata (the git sha, document url, the document source and a
     concatenation of the headers) and Weaviate will create the vectors.
 
-    Any existing documents are not upserted.  The assumption is that this is a first
+    Any existing documents are skipped.  The assumption is that this is a first
     import of data and skipping upsert checks will speed up import.
 
     param dfs: A list of dataframes from downstream dynamic tasks
@@ -66,12 +70,16 @@ def import_data(dfs: list[pd.DataFrame], class_name: str) -> list:
 
     df["uuid"] = df.apply(lambda x: generate_uuid5(identifier=x.to_dict(), namespace=class_name), axis=1)
 
+    if df[["docLink", "uuid"]].duplicated().any():
+        df.drop_duplicates(subset=["docLink", "uuid"], keep="first", inplace=True)
+        df.reset_index(drop=True, inplace=True)
+
     print(f"Passing {len(df)} objects for import.")
 
     return {
         "data": df,
         "class_name": class_name,
-        "upsert": False,
+        "existing": "skip",
         "uuid_column": "uuid",
         "error_threshold": 0,
         "batched_mode": True,
@@ -89,8 +97,8 @@ def import_baseline(class_name: str, seed_baseline_url: str) -> list:
 
     seed_baseline_url is a URI for a parquet file of pre-embedded data.
 
-    Any existing documents are not upserted.  The assumption is that this is a first import of data and skipping
-    upsert checks will speed up import.
+    Any existing documents are replaced.  The assumption is that this is a first import of data and older data
+    should be removed.
 
     param class_name: The name of the class to import data.  Class should be created with weaviate schema.
     type class_name: str
@@ -114,7 +122,7 @@ def import_baseline(class_name: str, seed_baseline_url: str) -> list:
     return {
         "data": df,
         "class_name": class_name,
-        "upsert": False,
+        "existing": "replace",
         "uuid_column": "id",
         "embedding_column": "vector",
         "error_threshold": 0,
