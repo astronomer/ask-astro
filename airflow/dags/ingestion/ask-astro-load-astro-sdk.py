12129c6baa782873785ae9c80487a9fe057a019d
Pankaj <pankaj.singh@astronomer.io>
2023-11-30 16:01:20 +0530

    Add astronomer-provider and astro-sdk ingestions DAG

---
+++ airflow/dags/ingestion/ask-astro-load-astro-sdk.py
@@ -0,0 +1,49 @@
+import datetime
+import os
+
+from include.tasks.extract.astro_sdk_docs import extract_astro_sdk_docs
+from include.tasks.extract.utils.weaviate.ask_astro_weaviate_hook import AskAstroWeaviateHook
+
+from airflow.decorators import dag, task
+
+ask_astro_env = os.environ.get("`ASK_ASTRO_ENV", "dev")
+
+_WEAVIATE_CONN_ID = f"weaviate_{ask_astro_env}"
+WEAVIATE_CLASS = os.environ.get("WEAVIATE_CLASS", "DocsDev")
+ask_astro_weaviate_hook = AskAstroWeaviateHook(_WEAVIATE_CONN_ID)
+
+blog_cutoff_date = datetime.date(2023, 1, 19)
+
+default_args = {"retries": 3, "retry_delay": 30}
+
+schedule_interval = "0 5 * * *" if ask_astro_env == "prod" else None
+
+
+@task
+def get_astro_sdk_content():
+    dfs = extract_astro_sdk_docs()
+    return dfs
+
+
+@dag(
+    schedule_interval=schedule_interval,
+    start_date=datetime.datetime(2023, 9, 27),
+    catchup=False,
+    is_paused_upon_creation=True,
+    default_args=default_args,
+)
+def ask_astro_load_astro_sdk():
+    _import_data = (
+        task(ask_astro_weaviate_hook.ingest_data, retries=10)
+        .partial(
+            class_name=WEAVIATE_CLASS,
+            existing="upsert",
+            doc_key="docLink",
+            batch_params={"batch_size": 1000},
+            verbose=True,
+        )
+        .expand(dfs=get_astro_sdk_content())
+    )
+
+
+ask_astro_load_astro_sdk()
---
+++ airflow/include/tasks/extract/astronomer_providers_docs.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+import pandas as pd
+
+from include.tasks.extract.utils.html_url_extractor import extract_internal_url, url_to_df
+
+
+def extract_provider_docs() -> list[pd.DataFrame]:
+    exclude_docs = ["_api", "_modules", "_sources", "changelog.html", "genindex.html", "py-modindex.html", "#"]
+    base_url = "https://astronomer-providers.readthedocs.io/en/stable/"
+
+    urls = extract_internal_url(base_url, exclude_docs)
+    df = url_to_df(urls)
+    return [df]
