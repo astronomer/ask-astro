ad905e09ad91497e319ce972c2fed58ea414424c
Pankaj Singh <98807258+pankajastro@users.noreply.github.com>
2023-12-18 14:40:12 +0530

    Ingest astronomer-provider/astro-sdk html docs (#199)
    closes: https://github.com/astronomer/ask-astro/issues/166
    closes: https://github.com/astronomer/ask-astro/issues/165

--- airflow/dags/ingestion/ask-astro-load.py
+++ airflow/dags/ingestion/ask-astro-load.py
@@ -9,6 +9,8 @@ from pathlib import Path
 import pandas as pd
 from include.tasks import split
 from include.tasks.extract import airflow_docs, astro_cli_docs, blogs, github, registry, stack_overflow
+from include.tasks.extract.astro_sdk_docs import extract_astro_sdk_docs
+from include.tasks.extract.astronomer_providers_docs import extract_provider_docs
 from include.tasks.extract.utils.weaviate.ask_astro_weaviate_hook import AskAstroWeaviateHook
 
 from airflow.decorators import dag, task
@@ -145,6 +147,9 @@ def ask_astro_load_bulk():
                 "extract_astro_blogs",
                 "extract_github_python",
                 "extract_astro_registry_dags",
+                "extract_astro_cli_docs",
+                "extract_astro_sdk_doc",
+                "extract_astro_provider_doc",
             }
 
     @task(trigger_rule="none_failed")
@@ -200,6 +205,28 @@ def ask_astro_load_bulk():
         except Exception:
             df = astro_cli_docs.extract_astro_cli_docs()[0]
             df.to_parquet(astro_cli_parquet_path)
+
+        return [df]
+
+    @task(trigger_rule="none_failed")
+    def extract_astro_sdk_doc():
+        astro_sdk_parquet_path = "include/data/astronomer/docs/astro-sdk.parquet"
+        try:
+            df = pd.read_parquet(astro_sdk_parquet_path)
+        except Exception:
+            df = extract_astro_sdk_docs()[0]
+            df.to_parquet(astro_sdk_parquet_path)
+
+        return [df]
+
+    @task(trigger_rule="none_failed")
+    def extract_astro_provider_doc():
+        astro_provider_parquet_path = "include/data/astronomer/docs/astro-provider.parquet"
+        try:
+            df = pd.read_parquet(astro_provider_parquet_path)
+        except Exception:
+            df = extract_provider_docs()[0]
+            df.to_parquet(astro_provider_parquet_path)
 
         return [df]
 
@@ -289,6 +316,8 @@ def ask_astro_load_bulk():
     code_samples = extract_github_python.expand(source=code_samples_sources)
     _airflow_docs = extract_airflow_docs()
     _astro_cli_docs = extract_astro_cli_docs()
+    _extract_astro_sdk_docs = extract_astro_sdk_doc()
+    _extract_astro_providers_docs = extract_astro_provider_doc()
 
     _get_schema = get_schema_and_process(schema_file="include/data/schema.json")
     _check_schema = check_schema(class_objects=_get_schema)
@@ -303,7 +332,7 @@ def ask_astro_load_bulk():
         registry_cells_docs,
     ]
 
-    html_tasks = [_airflow_docs, _astro_cli_docs]
+    html_tasks = [_airflow_docs, _astro_cli_docs, _extract_astro_sdk_docs, _extract_astro_providers_docs]
 
     python_code_tasks = [registry_dags_docs, code_samples]
 
---
+++ airflow/include/tasks/extract/astro_sdk_docs.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import logging
+
+import pandas as pd
+
+from include.tasks.extract.utils.html_url_extractor import extract_internal_url, url_to_df
+
+logger = logging.getLogger("airflow.task")
+
+
+def extract_astro_sdk_docs() -> list[pd.DataFrame]:
+    exclude_docs = ["autoapi", "genindex.html", "py-modindex.html", ".md", ".py"]
+    base_url = "https://astro-sdk-python.readthedocs.io/en/stable/"
+
+    urls = extract_internal_url(base_url, exclude_docs)
+
+    new_urls = [url for url in urls if "stable" in url]
+    logger.info("******ingesting****")
+    logger.info(new_urls)
+    logger.info("*********************")
+    df = url_to_df(new_urls, "astro-sdk")
+
+    return [df]
