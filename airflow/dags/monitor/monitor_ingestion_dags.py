5810ed9799e77eadffbdc7a82123531d3d132c1f
Pankaj <pankaj.singh@astronomer.io>
2023-12-20 20:35:24 +0530

    Fix static check

--- airflow/dags/monitor/monitor_ingestion_dags.py
+++ airflow/dags/monitor/monitor_ingestion_dags.py
@@ -19,14 +19,12 @@ ingestion_dags = [
     "ask_astro_load_airflow_docs",
     "ask_astro_load_astro_cli_docs",
     "ask_astro_load_astronomer_providers",
-    "ask_astro_load_astro_sdk"
-    "ask_astro_load_blogs",
+    "ask_astro_load_astro_sdk" "ask_astro_load_blogs",
     "ask_astro_load_bulk",
     "ask_astro_load_github",
     "ask_astro_load_registry",
     # "ask_astro_load_slack",
     "ask_astro_load_stackoverflow",
-    ""
 ]
 
 
