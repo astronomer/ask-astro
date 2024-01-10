c680cc9a74e0f1686b4f03b099794be1729983e3
David Xue <david.xue@astronomer.io>
2024-01-09 17:58:16 -0800

    Change MultiQuery Prompt, Add Hybrid Search (BM25 + Embedding), Cohere Reranker & LLM Chain Filter (#247)
    ### Description
    - Changed MultiQueryRetriever's prompt and parameter so we now keep
    user's original query + 2 reworded queries (whereas 3 different reworded
    queries)
    - Added Hybrid Search with alpha = 0.5 (equal weight between BM25/TF-TID
    & embedding model) and retrieving 100 documents for each prompt
    - Added Cohere Reranker (keeping 10 documents) after documents are
    combined from the 3 prompts from multi-query retriever
    - Added LLM Chain Filter (using GPT 3.5) to double check each remaining
    10 documents is relevant to the question asked
    - This essentially uses GPT 3.5 to ask if document X is relevant to the
    question (with a YES/NO) response
    - Added our custom boolean parser as the default one in LangChain can be
    too strict causing error raising in specific cases
    
    ### Technical Changes
    - Added api/ask_astro/chains/custom_llm_filter_prompt.py because the
    default implementation of boolean parser in LangChain library enforces
    that the GPT-3.5 used must either output "YES" or "NO" or else it raises
    an error. I essentially took their boolean parser from
    [here](https://github.com/langchain-ai/langchain/blob/master/libs/langchain/langchain/output_parsers/boolean.py)
    and re-implemented such that 1. we only check YES/NO is CONTAINED in the
    response (as sometimes LLM can output "YES.") 2. If neither, then don't
    raise an error but just return True (in our use case we just don't
    filter out/still consider this doc relevant).
    - Main changes are in api/ask_astro/chains/answer_question.py
    - Added cohere package in api/pyproject.toml
    
    ### Notes
    - Need further more extensive evaluation and comparison on a full Q&A
    dataset to determine improvements (and potential regressions).
    - My initial test/evaluation of the same 74 questions that my teammates
    have used in the past can be found on [langsmith
    here](https://smith.langchain.com/o/4942ae8b-e5be-4964-bb1f-7544886fa06d/projects/p/26697fd3-75a4-4056-8fb4-9a1b10420dec?columnVisibilityModel=%7B%22name%22%3Afalse%2C%22start_time%22%3Afalse%2C%22total_tokens%22%3Afalse%2C%22inputs%22%3Atrue%2C%22outputs%22%3Atrue%2C%22tags%22%3Afalse%2C%22in_dataset%22%3Afalse%2C%22last_queued_at%22%3Afalse%2C%22first_token_time%22%3Afalse%2C%22metadata%22%3Afalse%2C%22feedback_stats%22%3Afalse%7D)

--- api/ask_astro/chains/answer_question.py
+++ api/ask_astro/chains/answer_question.py
@@ -11,10 +11,14 @@ from langchain.prompts import (
     MessagesPlaceholder,
     SystemMessagePromptTemplate,
 )
-from langchain.retrievers import MultiQueryRetriever
+from langchain.prompts.prompt import PromptTemplate
+from langchain.retrievers import ContextualCompressionRetriever, MultiQueryRetriever
+from langchain.retrievers.document_compressors import CohereRerank, LLMChainFilter
+from langchain.retrievers.weaviate_hybrid_search import WeaviateHybridSearchRetriever
 
-from ask_astro.clients.weaviate_ import docsearch
-from ask_astro.config import AzureOpenAIParams
+from ask_astro.chains.custom_llm_filter_prompt import custom_llm_chain_filter_prompt_template
+from ask_astro.clients.weaviate_ import client
+from ask_astro.config import AzureOpenAIParams, CohereConfig, WeaviateConfig
 from ask_astro.settings import (
     CONVERSATIONAL_RETRIEVAL_LLM_CHAIN_DEPLOYMENT_NAME,
     CONVERSATIONAL_RETRIEVAL_LLM_CHAIN_TEMPERATURE,
@@ -32,19 +36,61 @@ with open("ask_astro/templates/combine_docs_chat_prompt.txt") as system_prompt_f
         HumanMessagePromptTemplate.from_template("{question}"),
     ]
 
+hybrid_retriever = WeaviateHybridSearchRetriever(
+    client=client,
+    index_name=WeaviateConfig.index_name,
+    text_key=WeaviateConfig.text_key,
+    attributes=WeaviateConfig.attributes,
+    k=WeaviateConfig.k,
+    alpha=WeaviateConfig.alpha,
+    create_schema_if_missing=WeaviateConfig.create_schema_if_missing,
+)
+
 # Initialize a MultiQueryRetriever using AzureChatOpenAI and Weaviate.
-retriever = MultiQueryRetriever.from_llm(
+user_question_rewording_prompt_template = PromptTemplate(
+    input_variables=["question"],
+    template="""You are an AI language model assistant. Your task is
+    to generate 2 different versions of the given user
+    question to retrieve relevant documents from a vector database.
+    By rewording the original question, expanding on abbreviated words if there are any,
+    and generating multiple perspectives on the user question,
+    your goal is to help the user overcome some of the limitations
+    of distance-based similarity search. Provide these alternative
+    questions separated by newlines. Original question: {question}""",
+)
+multi_query_retriever = MultiQueryRetriever.from_llm(
     llm=AzureChatOpenAI(
         **AzureOpenAIParams.us_east,
         deployment_name=MULTI_QUERY_RETRIEVER_DEPLOYMENT_NAME,
         temperature=MULTI_QUERY_RETRIEVER_TEMPERATURE,
     ),
-    retriever=docsearch.as_retriever(),
+    include_original=True,
+    prompt=user_question_rewording_prompt_template,
+    retriever=hybrid_retriever,
+)
+
+# Rerank
+cohere_reranker_compressor = CohereRerank(user_agent="langchain", top_n=CohereConfig.rerank_top_n)
+reranker_retriever = ContextualCompressionRetriever(
+    base_compressor=cohere_reranker_compressor, base_retriever=multi_query_retriever
+)
+
+# GPT-3.5 to check over relevancy of the remaining documents
+llm_chain_filter = LLMChainFilter.from_llm(
+    AzureChatOpenAI(
+        **AzureOpenAIParams.us_east,
+        deployment_name=CONVERSATIONAL_RETRIEVAL_LLM_CHAIN_DEPLOYMENT_NAME,
+        temperature=0.0,
+    ),
+    custom_llm_chain_filter_prompt_template,
+)
+llm_chain_filter_compression_retriever = ContextualCompressionRetriever(
+    base_compressor=llm_chain_filter, base_retriever=reranker_retriever
 )
 
 # Set up a ConversationalRetrievalChain to generate answers using the retriever.
 answer_question_chain = ConversationalRetrievalChain(
-    retriever=retriever,
+    retriever=llm_chain_filter_compression_retriever,
     return_source_documents=True,
     question_generator=LLMChain(
         llm=AzureChatOpenAI(
