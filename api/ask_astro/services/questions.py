0128617560b51dfd81f376bc91bd1b7a1c83b316
David Xue <xuegdxw@gmail.com>
2024-01-25 15:49:27 -0800

    Add custom error types and change error propagation

--- api/ask_astro/services/questions.py
+++ api/ask_astro/services/questions.py
@@ -6,13 +6,21 @@ import re
 import time
 from logging import getLogger
 
-from tenacity import retry, stop_after_attempt, wait_exponential
+from tenacity import retry, retry_if_not_exception_type, stop_after_attempt, wait_exponential
 
 from ask_astro.clients.firestore import firestore_client
 from ask_astro.config import FirestoreCollections
 from ask_astro.models.request import AskAstroRequest, Source
 
 logger = getLogger(__name__)
+
+
+class InvalidRequestPromptError(Exception):
+    """Exception raised when the prompt string in the request object is invalid"""
+
+
+class QuestionAnsweringError(Exception):
+    """Exception raised when an error occurs during question answering"""
 
 
 async def _update_firestore_request(request: AskAstroRequest) -> None:
@@ -32,17 +40,23 @@ def _preprocess_request(request: AskAstroRequest) -> None:
     if len(request.prompt) > 20000:
         error_msg = "Question text is too long. Please try making a new thread and shortening your question."
         request.response = error_msg
-        raise Exception(error_msg)
+        raise InvalidRequestPromptError(error_msg)
     if len(request.prompt) == 0:
         error_msg = "Question text cannot be empty. Please try again with a different question."
         request.response = error_msg
-        raise Exception(error_msg)
+        raise InvalidRequestPromptError(error_msg)
+    # take the most recent 10 question and answers in the history
     if len(request.messages) > 10:
         request.messages = request.messages[-10:]
+    # parse out backslack escape character to prevent hybrid search erroring out with invalid syntax string
     request.prompt = re.sub(r"(?<!\\)\\(?!\\)", "", request.prompt)
 
 
-@retry(stop=stop_after_attempt(5), wait=wait_exponential(multiplier=1, max=10))
+@retry(
+    retry=retry_if_not_exception_type(InvalidRequestPromptError),
+    stop=stop_after_attempt(5),
+    wait=wait_exponential(multiplier=1, max=10),
+)
 async def answer_question(request: AskAstroRequest) -> None:
     """
     Performs the actual question answering logic and updates the request object.
@@ -87,15 +101,14 @@ async def answer_question(request: AskAstroRequest) -> None:
             if doc.metadata.get("docLink", "").startswith("https://")
         ]
 
-        await _update_firestore_request(request)
-
     except Exception as e:
         # If there's an error, mark the request as errored and add it to the database
         request.status = "error"
-        if request.response == "":
+        if not isinstance(e, InvalidRequestPromptError):
             request.response = "Sorry, something went wrong. Please try again later."
+            raise QuestionAnsweringError("An error occurred during question answering.") from e
+        else:
+            raise e
 
+    finally:
         await _update_firestore_request(request)
-
-        # Propagate the error
-        raise Exception("An error occurred during question answering.") from e
