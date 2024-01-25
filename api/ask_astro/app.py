836579aefdf2ce4279c830e53ffd6851e6277200
David Xue <david.xue@astronomer.io>
2024-01-25 14:16:49 -0800

    Add sveltekit-rate-limiter & Fix Rate Limiter Issue (#280)
    ### Description
    - See https://github.com/astronomer/ask-astro/issues/268 for details of
    why the error occurs. This won't be explained again in this PR.
    - TL;DR: svelte backend (cloudflare) is sitting in front of the GCP API
    therefore it is not possible to do IP based rate limiting in sanic.
    Instead, we must implement it in svelte for now.
    - In production environment, I have set cookie rate limit to be 5
    requests per minute, and IP based rate limit to be 100 per day using
    cloudflare's environment varables
    
    ### Technical Changes
    - Add sveltekit-rate-limiter
    - Revert the old PR that caused the rate limit issue (where all requests
    are misinterpreted as the same IP)
    
    ### Tests
    Setup:
    - In Cloudflare, I have set the dev environment to temporarily have
    these environment varaibles
    
    ![image](https://github.com/astronomer/ask-astro/assets/26350341/e054d730-08eb-4ce5-896c-df646c42cb6f)
    
    1. Test cookie rate limiter. This should trigger after 5 requests using
    the same browser
    
    
    https://github.com/astronomer/ask-astro/assets/26350341/0431cae3-b38d-479c-9b87-b1e7667286bd
    
    2. Using incognito, we can continue to send requests without being
    blocked. But after 5 more requests we hit our IP based rate limit
    <img width="1130" alt="image"
    src="https://github.com/astronomer/ask-astro/assets/26350341/c56b3a78-4fb2-4e6b-a891-f4e1ed037ba8">
    
    3. Now, just to be safe, I used a different computer with VPN and
    reconnected to the same dev website. And confirmed that I can continue
    to send additional requests
    
    closes #268

--- api/ask_astro/app.py
+++ api/ask_astro/app.py
@@ -5,12 +5,9 @@ import logging
 import os
 from logging import getLogger
 
-from sanic import Request, Sanic, json
-from sanic_limiter import Limiter, get_remote_address
-from sanic_limiter.errors import RateLimitExceeded
+from sanic import Request, Sanic
 
 from ask_astro.rest.controllers import register_routes
-from ask_astro.rest.controllers.post_request import on_post_request
 from ask_astro.slack.app import app_handler, slack_app
 from ask_astro.slack.controllers import register_controllers
 
@@ -20,15 +17,6 @@ logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = getLogger(__name__)
 
 api = Sanic(name="ask_astro")
-
-limiter = Limiter(
-    api,
-    key_func=get_remote_address,
-)
-requests_per_day = os.environ.get("API_REQUESTS_PER_DAY", "50")
-requests_per_hour = os.environ.get("API_REQUESTS_PER_HOUR", "20")
-requests_per_minute = os.environ.get("API_REQUESTS_PER_MINUTE", "10")
-requests_limit_value = f"{requests_per_day} per day, {requests_per_hour} per hour, {requests_per_minute} per minute"
 
 
 # route slack requests to the slack app
@@ -40,29 +28,10 @@ async def endpoint(req: Request):
     return await app_handler.handle(req)
 
 
-@api.exception(RateLimitExceeded)
-async def catch_rate_limit(request, exception):
-    logger.warning("Rate limit exceeded for IP %s at endpoint %s", request.ip, request.path)
-    return json(
-        {"error": "Rate limit exceeded"},
-        status=429,
-    )
-
-
 server_port = int(os.environ.get("PORT", 8080))
 
 register_controllers(slack_app)
 register_routes(api)
 
-
-# We want to rate limit requests to the /requests endpoint, but not to the other endpoints.
-# Hence, we use a separate explicit route registration here.
-@api.post("/requests", name="post_request")
-@limiter.limit(requests_limit_value, key_func=get_remote_address)
-async def post_request(req: Request):
-    """Handle POST requests to the /requests endpoint."""
-    return await on_post_request(req)
-
-
 if __name__ == "__main__":
-    api.run(host="0.0.0.0", port=server_port, auto_reload=True)
+    api.run(host="0.0.0.0", port=server_port)
