fbbce961c46bf25860a1a2e5d08fafedbf228893
Carter Fulcher <fulcher.carter@gmail.com>
2024-02-22 20:52:27 -0500

    Update api/ask_astro/__init__.py
    Co-authored-by: Wei Lee <weilee.rx@gmail.com>

--- api/ask_astro/__init__.py
+++ api/ask_astro/__init__.py
@@ -1,4 +1,4 @@
 import os
 from dotenv import load_dotenv
  
-load_dotenv(os.path.normpath(os.getcwd() + os.sep + os.pardir + '/.env'))
+load_dotenv(os.path.normpath(f"{os.getcwd()}{os.sep}{os.pardir}/.env"))
