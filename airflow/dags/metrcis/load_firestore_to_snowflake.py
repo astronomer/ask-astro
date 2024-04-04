953160d8e70e99208547fb150bba9cf697490e72
David Xue <xuegdxw@gmail.com>
2024-04-04 11:00:25 -0400

    Fix incorrect type hinting

--- airflow/dags/metrcis/load_firestore_to_snowflake.py
+++ airflow/dags/metrcis/load_firestore_to_snowflake.py
@@ -6,11 +6,13 @@ to snowflake.
 import os
 import time
 from datetime import date, datetime, timedelta
+from uuid import UUID
 
 import snowflake.connector
 from google.cloud import firestore
 from google.cloud.firestore_v1.base_query import FieldFilter
 from include.utils.slack import send_failure_notification
+from langchain.schema import BaseMessage
 
 from airflow.decorators import dag, task
 
@@ -38,7 +40,9 @@ snowflake.connector.paramstyle = "qmark"
 )
 def load_firestore_to_snowflake():
     @task()
-    def load_request_data_from_firestore() -> list[tuple[str, int, bool, int]]:
+    def load_request_data_from_firestore() -> (
+        list[tuple[UUID, int | None, str, int, str | None, str, str | None, list[BaseMessage]]]
+    ):
         firestore_client = firestore.Client(project="ask-astro")
 
         requests_col = firestore_client.collection(FIRESTORE_REQUESTS_COLLECTION)
@@ -54,7 +58,7 @@ def load_firestore_to_snowflake():
             .stream()
         )
 
-        rows: list[tuple[str, int | None, bool, datetime]] = []
+        rows: list[tuple[UUID, int | None, str, int, str | None, str, str | None, list[BaseMessage]]] = []
         for doc in docs:
             doc_dict = doc.to_dict()
             uuid = doc_dict["uuid"]
